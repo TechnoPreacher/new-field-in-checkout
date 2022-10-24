7ab76a25217efd9b09cb7958568a86ca3726f906
TechnoPreacher <nik.nik.sulima@gmail.com>
2022-10-25 01:30:53 +0300

    first atempt

--- field-in-checkout.php
+++ field-in-checkout.php
@@ -12,8 +12,8 @@
  * Requires PHP: 7.4
 */
 
-add_action( 'plugins_loaded', 'field_in_checkout_loaded' );//подключаем регистратор
-add_action( 'save_post_shop_order', 'my_extra_fields2_update', 0 );//обновление полей при сохранении заказа(!) Вукомерс
+//add_action( 'plugins_loaded', 'field_in_checkout_loaded' );//подключаем регистратор
+add_action( 'save_post_shop_order', 'field_in_checkout_update', 0 );//обновление полей при сохранении заказа(!) Вукомерс
 add_filter( 'manage_edit-shop_order_columns', 'customsFields' );//как можно, находясь в здравом уме, это узнать, если post_type=shop_order????
 add_action( 'manage_posts_custom_column', 'customsFieldsDataOutput', 10, 2 );//а тут, надо же, стандартно!
 add_action( 'wp_enqueue_scripts', 'add_scripts' );//хук для подключения скрипта
@@ -22,105 +22,95 @@ add_action( 'wp_enqueue_scripts', 'add_scripts' );//хук для подключ
 //add_action( 'woocommerce_email_footer', 'insert_custom_value_in_email', 10, 1 );//так можно в футер
 add_action( 'woocommerce_email_after_order_table', 'insert_custom_value_in_email', 10, 4 );//ну а так после таблицы
 
-register_deactivation_hook( __FILE__, 'field_in_checkout_deactivate' );//убираю всё что сделал плагин
+//register_deactivation_hook( __FILE__, 'field_in_checkout_deactivate' );//убираю всё что сделал плагин
 
-
-function my_extra_fields2_update( $post_id )//Сохрание маета-данных, при сохранении поста
+function field_in_checkout_update( $post_id )//Сохрание маета-данных, при сохранении поста
 {
-
 // базовая проверка
-	if (
-		empty( $_POST['order_comments2'] )//order_comments2
-		//|| !wp_verify_nonce($_POST['extra_fields_nonce'], __FILE__) - это защита!
+	if (//тут не помешает проверка правильности отправителя данных
+		empty( $_POST['field_in_checkout_metadata'] )
 		|| wp_is_post_autosave( $post_id )
 		|| wp_is_post_revision( $post_id )
 	) {
 		return false;
 	}
-	// Все ОК! Теперь, нужно сохранить/удалить данные
 
-	$_POST['order_comments2'] = sanitize_text_field( $_POST['order_comments2'] );
+	$_POST['field_in_checkout_metadata'] = sanitize_text_field( $_POST['field_in_checkout_metadata'] );
 
-	update_post_meta( $post_id, 'order_comments2',
-		$_POST['order_comments2'] ); // add_post_meta() работает автоматически
-	//}
+	update_post_meta( $post_id, 'field_in_checkout_metadata',
+		$_POST['field_in_checkout_metadata'] ); // add_post_meta() работает автоматически
+
 	return $post_id;
-
 }
 
 function customsFields( $columns ) {//вывод значений мета-полей в общем списке в админке!
 	$my_columns = [//идентификатор колонки
-		'status' => '<p style=\'border: 3px solid red\'>' .
+		'field_in_checkout_metadata' => '<p style=\'border: 3px solid pink\'>' .
 		            __( 'Костюмное поле', 'field-in-checkout' ) . '</p>',
 	];
-
 	return $my_columns + $columns;//кастомная колонка впереди
 }
-
 
 //данные! - нужно проверить есть ли они
 function customsFieldsDataOutput( $column ) {
 
 	$custom_fields = get_post_custom();//мета-данные поста
 
-	$my_custom_field = $custom_fields['order_comments2'][0] ?? null;//если есть - ок, нет - пустота
+	$my_custom_field = $custom_fields['field_in_checkout_metadata'][0] ?? null;//если есть - ок, нет - пустота
 	$strstyle        = '';
 	if ( isset( $my_custom_field ) ) {
-		$strstyle = '<p style=\'border: 1px solid yellow\'>';
+		$strstyle = '<p style=\'border: 2px solid deeppink\'>';
 	}
 	switch ( $column ) {//выбираю в какой столбец админки сунуть данные!
-		case 'status' ://имя колонки - задаётся при формировании её заголовка в customsFields($columns) !!!
+		case 'field_in_checkout_metadata' ://имя колонки - задаётся при формировании её заголовка в customsFields($columns) !!!
 			echo $strstyle . __( $my_custom_field,
 					'field-in-checkout' ) . '</p>';// " <p style=\'border: 5px solid red\'>ояние события</p>";
 			break;
 	}
 }
 
-
 function add_scripts() {
-	if ( is_checkout() ) { //атработает при выведении страниц чекаута - сюда инжектировать метаданные дял отображения на фронте!
+	/*
+	отработает при выведении страниц чекаута - сюда инжектировать метаданные дял отображения на фронте!
+	это может быть как страница подтверждения заказа, так и страница с инфой о доставке заказа
+	в первой случае не будет $wp->query_vars['order-received'] !!
+	*/
+	if ( is_checkout() ) {
 
 		wp_enqueue_script( 'field-in-checkout-js',
 			plugins_url( '/js/field-in-checkout.js', __FILE__ ),//путь к жс-скрипту относительно этого пхп-файла!
-			array( 'jquery' ) );//подлючаю JS-скрипт и говорю что он зависим от JQuery
-		//это может быть как страница подтверждения заказа, так и страница с инфой о доставке заказа
-		//в первой случае не будет $wp->query_vars['order-received']
+			array( 'jquery' )
+		);//подлючаю JS-скрипт и говорю что он зависим от JQuery
 
-		global $wp; // $wp->query_vars['order-received'];
+		global $wp; //чтоб иметь доступ к глобальным данным
 
 		$custom_field = '';
+
 		if ( isset( $wp->query_vars['order-received'] ) ) {
-			if ( isset( get_post_custom( $wp->query_vars['order-received'] )['order_comments2'] ) ) {
-				$custom_field = get_post_custom( $wp->query_vars['order-received'] )['order_comments2'][0];
+			if ( isset( get_post_custom( $wp->query_vars['order-received'] )['field_in_checkout_metadata'] ) ) {
+				$custom_field = get_post_custom( $wp->query_vars['order-received'] )['field_in_checkout_metadata'][0];
 			}
 		}
 
 		$variables = array(
-			'orde233' => $custom_field //путь к скрипту-обработчитку аякс-запросов r_comments_field2
+			'value' => $custom_field, //кастомное значение
+			'label'=>__('Метаданные','field-in-checkout'), //переводимы заголовок кастомного поля
 		);
 
-		//передача значения переменной  на фронт внутри объекта. доступ в ЖС такой: window.field_in_checkout.orde233;
+		//передача значения переменной  на фронт внутри объекта. доступ в ЖС такой: window.field_in_checkout.field-in-checkout;
 		wp_localize_script( 'field-in-checkout-js', 'field_in_checkout', $variables );
 	};
 }
 
-
 function insert_custom_value_in_email( $order, $sent_to_admin, $plain_text, $email ) {
-	echo '<div  style=\'border: 3px dotted aquamarine\'>
-                <h4> Дополнительная информация: </h4> 
-                <p>' . $_POST['order_comments2'] . '</p>
+	echo '<div  style=\'border: 3px dotted hotpink\'>
+                <h4>'. __('Дополнительная информация','field-in-checkout').':'. '</h4> 
+                <p>' . $_POST['field_in_checkout_metadata'] . '</p>
           </div>
           <br>';
 }
 
+//function field_in_checkout_loaded() {}
 
-function field_in_checkout_loaded() {
-
-
-
-
-}
-
-function field_in_checkout_deactivate() {
-//	unregister_widget( 'ajax_filter_widget' );//убить виджет
-}
+//function field_in_checkout_deactivate() {
+//	unregister_widget( 'ajax_filter_widget' );//убить виджет}
--- js/field-in-checkout.js
+++ js/field-in-checkout.js
@@ -1,52 +1,33 @@
 jQuery(function () {
 
-
    // моё поле в чекауте
-    let html = ' <div class="woocommerce-additional-fields__field-wrapper">\n' +
-        '    <p class="form-row notes" id="order_comments_field2" data-priority="">\n' +
-        '        <label for="order_comments2" class="">\n' +
-        '            Примечание к заказу&nbsp;\n' +
-        '            <span class="optional">(необязательно)</span>\n' +
-        '        </label>\n' +
-        '        <span class="woocommerce-input-wrapper">\n' +
-        '            <textarea name="order_comments2" class="input-text " id="order_comments2" placeholder="Примечания к вашему заказу, например, особые пожелания отделу доставки." rows="2" cols="5"></textarea>\n' +
-        '        </span>\n' +
-        '    </p>\n' +
+    let html = ' <div class="woocommerce-additional-fields__field-wrapper">' +
+        '    <p class="form-row notes" id="field_in_checkout_metadata_p" data-priority="">' +
+        '        <label for="field_in_checkout_metadata" class="">' +
+        '            Кастомное поле' +
+        '            <span class="optional">(_необязательно_)</span>' +
+        '        </label>' +
+        '        <span class="woocommerce-input-wrapper">' +
+        '            <textarea name="field_in_checkout_metadata" class="input-text " id="field_in_checkout_metadata" placeholder="Введи сюда что-то важное!" rows="2" cols="5"></textarea>' +
+        '        </span>' +
+        '    </p>' +
         '</div>';
 
     let elPosts = jQuery('.form-row.notes');//нахожу базовое поле с комментарием
-elPosts.after(html);//добавляю под него своё
-    jQuery('#order_comments_field2').css('border', '5px solid red');
 
-    //инфа из моего поля в финальном чекауте
-   // class="woocommerce-table woocommerce-table--order-details shop_table order_details"
-  //  console.log(Object.keys(window));
-  //  let valuevich ='90';//window.field_in_checkout.order_comments_field2;
-    //let html2 =    '<tr> <th id="custom">днище:</th> <td id="custom2">'+valuevich+'</td></tr>';
+    elPosts.after(html);//добавляю под него своё
+    jQuery('#field_in_checkout_metadata_p').css('border', '2px solid pink');
 
-  //  let html2 ='<p>SSSS</p>';
-//let x = jQuery('.woocommerce-table.woocommerce-table--order-details.shop_table.order_details tr');
-//    x.last().after(html2);
+    let field_in_checkout_value =window.field_in_checkout.value;//значение
+    let field_in_checkout_label =window.field_in_checkout.label;//подпись (так чтоб был перевод!)
 
-    //jQuery('.woocommerce-table.woocommerce-table--order-details.shop_table.order_details tr th').css('border', '5px solid red');
+    //если есть кастомное значение - тащу оттуда
+    if (field_in_checkout_value !== '') {
+    let html2 =    '<tr> <th id="custom">'+field_in_checkout_label+':</th> <td id="custom2">'+ field_in_checkout_value+'</td></tr>';
 
-
-
+    let custom_tr = jQuery('.woocommerce-table.woocommerce-table--order-details.shop_table.order_details tr');
+        custom_tr.last().after(html2);
+    jQuery('#custom , #custom2').css('border', '2px dotted pink');
+    }
 
 });
-
-jQuery(function() {//надо для  первой фильтрации при открытии страницы и с подхватом значения из виджета!
-  //  console.log(Object.values(window));
-
-    let valuevich =window.field_in_checkout.orde233;
-    let html2 =    '<tr> <th id="custom">днище:</th> <td id="custom2">'+valuevich+'</td></tr>';
-
-    //  let html2 ='<p>SSSS</p>';
-    let x = jQuery('.woocommerce-table.woocommerce-table--order-details.shop_table.order_details tr');
-    x.last().after(html2);
-    jQuery('#custom , #custom2').css('border', '2px dotted red');
-
-});
-
-
-
